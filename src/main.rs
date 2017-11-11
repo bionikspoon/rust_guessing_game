93cd94892402ec4eb1993d7f9b41d459b471af26
Manu Phatak <manu@thinkcerca.com>
2017-11-11 15:33:08 -0600

    generics with trains

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ fn main() {
 
     println!("The largest number is {}", largest(&number_list));
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    println!("The largest number is {}", largest(&number_list));
+    println!("The largest number is {}", largest(&char_list));
 }
 
-fn largest(list: &[i32]) -> i32 {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
-    for &number in list {
+    for &number in list.iter() {
         if number > largest {
             largest = number;
         }
