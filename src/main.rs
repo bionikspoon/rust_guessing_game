490e8311eca95216cae8257dac33a5589f2181cd
Manu Phatak <manu@thinkcerca.com>
2017-11-11 10:15:27 -0600

    matching coins

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 #[derive(Debug)]
-enum Maybe<T> {
-    Just(T),
-    Nothing,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+impl Coin {
+    fn value(&self) -> u32 {
+        match self {
+            &Coin::Penny => 1,
+            &Coin::Nickel => 5,
+            &Coin::Dime => 10,
+            &Coin::Quarter => 25,
+        }
+    }
 }
 
 fn main() {
-    let maybe_number = Maybe::Just(5);
-    let maybe_string = Maybe::Just("string");
-
-    let missing_number: Maybe<i32> = Maybe::Nothing;
-
-    println!("{:?}", maybe_number);
-    println!("{:?}", maybe_string);
-    println!("{:?}", missing_number);
-
-    let x: i8 = 5;
-    let y: Option<i8> = Some(5);
-
-    match y {
-        None => unimplemented!(),
-        Some(y) => x + y,
-    };
+    println!("Penny value: {:?}", Coin::Penny.value());
+    println!("Nickel value: {:?}", Coin::Nickel.value());
+    println!("Dime value: {:?}", Coin::Dime.value());
+    println!("Quarter value: {:?}", Coin::Quarter.value());
 }
