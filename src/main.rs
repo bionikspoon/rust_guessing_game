0d439555a55a1ab17593d41e4d1205c7c69d4dce
Manu Phatak <manu@thinkcerca.com>
2017-11-11 09:21:16 -0600

    use structs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
 fn main() {
-    let mut s = String::from("hello");
 
-    s.push_str(", world");
+    let rect = Rectangle {
+        length: 50,
+        width: 30,
+    };
 
-    println!("{:?}", s);
+    println!("rect is {:#?}", rect);
+
+    println!(
+        "The area of the reactangle is {} square pixels.",
+        area(&rect)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
