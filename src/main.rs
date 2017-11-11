fc539cc2295fd221963925cd02b15e55131d033a
Manu Phatak <manu@thinkcerca.com>
2017-11-10 18:16:06 -0600

    ownership

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-extern crate rand;
-
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut s = String::from("hello");
 
-    loop {
-        println!("Please input your guess.");
+    s.push_str(", world");
 
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess).expect(
-            "Failed to read line",
-        );
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Expecting a number.");
-                continue;
-            }
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You Win");
-                break;
-            }
-        }
-    }
-
+    println!("{:?}", s);
 }
