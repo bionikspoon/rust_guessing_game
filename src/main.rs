ff713224aa9ac70e4c36f47d70aa0af34bc716bf
Manu Phatak <manu@thinkcerca.com>
2017-11-11 10:09:40 -0600

    maybe type

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #[derive(Debug)]
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-        println!("{:?}", self);
-    }
+enum Maybe<T> {
+    Just(T),
+    Nothing,
 }
 
 fn main() {
-    let message = Message::Write(String::from("hello"));
-    message.call();
+    let maybe_number = Maybe::Just(5);
+    let maybe_string = Maybe::Just("string");
+
+    let missing_number: Maybe<i32> = Maybe::Nothing;
+
+    println!("{:?}", maybe_number);
+    println!("{:?}", maybe_string);
+    println!("{:?}", missing_number);
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    match y {
+        None => unimplemented!(),
+        Some(y) => x + y,
+    };
 }
